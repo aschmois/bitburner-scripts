587c90b28a61c79632ce267fe195c29d1925273e
Andres Schmois <andres.schmois@gmail.com>
2022-07-08 18:29:15 -0400

    Make stats table look prettier

--- src/lib/easy-table.ts
+++ src/lib/easy-table.ts
@@ -2,40 +2,63 @@ import { Global } from '/lib/global'
 
 import EasyTable from 'easy-table'
 
-export type Opts = { pre?: string; post?: string; transform?: (str: string) => string }
+export type Opts = {
+  pre?: string
+  post?: string
+  pad?(val: string, width: number): string
+  transform?: (val: string) => string
+}
 export abstract class Printer {
   public static currency(g: Global, opts?: Opts) {
     return (val: number, width: number) => {
-      const str = applyOpts(g.n(val, '$0.00a'), opts)
-      return width ? EasyTable.padLeft(str, width) : str
+      return applyOpts(g.n(val, '$0.00a'), width, opts?.pad || padLeft, opts)
     }
   }
   public static number(g: Global, opts?: Opts) {
     return (val: number, width: number) => {
-      const str = applyOpts(g.n(val, '0,0'), opts)
-      return width ? EasyTable.padLeft(str, width) : str
+      return applyOpts(g.n(val, '0,0'), width, opts?.pad || padLeft, opts)
     }
   }
   public static nNumber(g: Global, opts?: Opts) {
     return (val: number, width: number) => {
-      const str = applyOpts(g.n(val, '0.00a'), opts)
-      return width ? EasyTable.padLeft(str, width) : str
+      return applyOpts(g.n(val, '0.00a'), width, opts?.pad || padLeft, opts)
     }
   }
   public static percent(g: Global, opts?: Opts) {
     return (val: number, width: number) => {
-      const str = applyOpts(g.n(val * 100, '0,0') + '%', opts)
-      return width ? EasyTable.padLeft(str, width) : str
+      return applyOpts(g.n(val * 100, '0,0') + '%', width, opts?.pad || padLeft, opts)
+    }
+  }
+  public static string(g: Global, opts?: Opts) {
+    return (val: string, width: number) => {
+      return applyOpts(val, width, opts?.pad || padRight, opts)
     }
   }
 }
 
-function applyOpts(_str: string, opts?: Opts) {
-  const pre = opts?.pre || ''
-  const post = opts?.post || ''
-  let str = _str
+function applyOpts(_val: string, _width: number, pad: (val: string, width: number) => string, opts?: Opts): string {
+  const pre = opts?.pre === undefined ? ' ' : opts.pre
+  const post = opts?.post === undefined ? ' ' : opts.post
+  let val = _val
   if (opts?.transform) {
-    str = opts.transform(str)
+    val = opts.transform(val)
   }
-  return pre + str + post
+  const width = _width - pre.length - post.length
+  return `${pre}${pad(val, width)}${post}`
+}
+
+export function padRight(val: string, _width: number) {
+  return val
+}
+
+export function padLeft(val: string, width: number) {
+  return EasyTable.padLeft(val, width)
+}
+
+export function padCol(table: EasyTable, col: string, minWidth: number, separator: string) {
+  table.cell(col, separator.repeat(Math.max(minWidth, col.length)))
+}
+
+export function removeSeparator(table: EasyTable) {
+  table.separator = '​' // Note the use of invisible character!
 }
--- src/stats.ts
+++ src/stats.ts
@@ -3,7 +3,7 @@ import { Global } from './lib/global'
 import { isHackable, scanForServers } from './lib/utils/scan'
 import { getWeightedServerValue } from './lib/utils/stats'
 import EasyTable from 'easy-table'
-import { Printer } from '/lib/easy-table'
+import { padCol, padRight, Printer, removeSeparator } from '/lib/easy-table'
 
 let g: Global
 export async function main(ns: NS) {
@@ -21,24 +21,56 @@ export async function main(ns: NS) {
       values.push(getWeightedServerValue(g, _server))
     }
     const table = new EasyTable()
+    removeSeparator(table)
+    enum Stats {
+      Hostname = ' hostname ',
+      MoneyPerSec = ' money/s ',
+      Threads = '* threads ',
+      TheoreticalMoneyPerSec = '= t$/s ',
+      Weight = '* wght ',
+      Adjustment = '+ adj ',
+      Value = '= value ',
+      Sep = ' | ',
+      Chance = ' chance ',
+      BaseDifficulty = ' diff ',
+      HackingLevelRequired = ' lvl ',
+      MoneyAvailable = ' $avail',
+      MoneyMax = '/$max ',
+    }
     for (const { log } of values) {
       const theoreticalMoneyPerS = log.moneyPerS * log.threads
-      table.cell('hostname', log.server.hostname)
-      table.cell('$/s', log.moneyPerS, Printer.currency(g, { post: '/s' }))
-      table.cell('threads', log.threads, Printer.number(g, { pre: '*' }))
-      table.cell('t$/s', theoreticalMoneyPerS, Printer.currency(g, { pre: '=', post: '/s' }))
-      table.cell('weight', log.weight, Printer.nNumber(g, { pre: '*' }))
-      table.cell('$max/1b', log.server.moneyMax / 1000000000, Printer.currency(g, { pre: '+' }))
-      table.cell('value', log.value, Printer.nNumber(g, { pre: '=' }))
-      table.cell('chance', log.chance, Printer.percent(g))
-      table.cell('diff', log.server.baseDifficulty, Printer.number(g))
-      table.cell('lvl', log.server.requiredHackingSkill, Printer.number(g))
-      table.cell('$avail', log.server.moneyAvailable, Printer.currency(g))
-      table.cell('$max', log.server.moneyMax, Printer.currency(g))
+      table.cell(Stats.Hostname, log.server.hostname, Printer.string(g))
+      table.cell(Stats.MoneyPerSec, log.moneyPerS, Printer.currency(g, { post: '/s ' }))
+      table.cell(Stats.Threads, log.threads, Printer.nNumber(g, { pre: '* ' }))
+      table.cell(Stats.TheoreticalMoneyPerSec, theoreticalMoneyPerS, Printer.currency(g, { pre: '= ', post: '/s ' }))
+      table.cell(Stats.Weight, log.weight, Printer.nNumber(g, { pre: '* ' }))
+      table.cell(Stats.Adjustment, log.server.moneyMax / 1000000000, Printer.currency(g, { pre: '+ ' }))
+      table.cell(Stats.Value, log.value, Printer.nNumber(g, { pre: '= ' }))
+      table.cell(Stats.Sep, '|', Printer.string(g))
+      table.cell(Stats.Chance, log.chance, Printer.percent(g))
+      table.cell(Stats.BaseDifficulty, log.server.baseDifficulty, Printer.number(g))
+      table.cell(Stats.HackingLevelRequired, log.server.requiredHackingSkill, Printer.number(g))
+      table.cell(Stats.MoneyAvailable, log.server.moneyAvailable, Printer.currency(g, { post: '' }))
+      table.cell(Stats.MoneyMax, log.server.moneyMax, Printer.currency(g, { pre: '/', pad: padRight }))
       table.newRow()
     }
+    table.sort([Stats.Value])
+    padCol(table, Stats.Hostname, 20, '-')
+    padCol(table, Stats.MoneyPerSec, 12, '-')
+    padCol(table, Stats.Threads, 10, '-')
+    padCol(table, Stats.TheoreticalMoneyPerSec, 13, '-')
+    padCol(table, Stats.Weight, 7, '-')
+    padCol(table, Stats.Adjustment, 10, '-')
+    padCol(table, Stats.Value, 10, '-')
+    padCol(table, Stats.Sep, 3, '-')
+    padCol(table, Stats.Chance, 6, '-')
+    padCol(table, Stats.BaseDifficulty, 4, '-')
+    padCol(table, Stats.HackingLevelRequired, 8, '-')
+    padCol(table, Stats.MoneyAvailable, 9, '-')
+    padCol(table, Stats.MoneyMax, 10, '-')
+    table.newRow()
     g.ns.clearLog()
-    g.printTable(table.sort(['value']))
+    g.printTable(table)
     if (a.con) await ns.sleep(100)
     else break
   }
